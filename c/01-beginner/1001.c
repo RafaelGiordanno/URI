795ebadaad1dd67359480992238cb16fd678adb8
Rafael Giordanno <rafael_giordanno@hotmail.com>
2017-06-09 19:01:36 -0300

    Problem 1001 solved.

---
+++ c/01-beginner/1001.c
@@ -0,0 +1,13 @@
+/** 
+ * link: https://www.urionlinejudge.com.br/judge/en/problems/view/1001 
+ * Author: Rafael Giordanno
+ * */
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("X = %d\n", (a + b));
+	return 0;
+}
