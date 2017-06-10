5f92d8b67182297f89e3bfb4713c82986821c334
Rafael Giordanno <rafael_giordanno@hotmail.com>
2017-06-10 11:24:19 -0300

    Problem 1002 solved.

---
+++ c/01-beginner/1002.c
@@ -0,0 +1,14 @@
+/** 
+ * link: https://www.urionlinejudge.com.br/judge/en/problems/view/1002
+ * Author: Rafael Giordanno
+ **/
+#include <stdio.h>
+#define n 3.14159
+
+int main()
+{
+	double r;
+	scanf("%lf", &r);
+	printf("A=%.4lf\n", (n * r * r));
+	return 0;
+}
