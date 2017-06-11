459707bc594b4e6402841a477f73a5e8adc9549a
Rafael Giordanno <rafael_giordanno@hotmail.com>
2017-06-11 19:54:23 -0300

    Problem 1007 solved in C. Updated gitignore.

---
+++ c/01-beginner/1007.c
@@ -0,0 +1,13 @@
+/**
+ * link: https://www.urionlinejudge.com.br/judge/en/problems/view/1007 
+ * Author: Rafael Giordanno
+ **/
+#include <stdio.h>
+
+int main()
+{
+	int a, b, c, d;
+	scanf("%d %d %d %d", &a, &b, &c, &d);
+	printf("DIFERENCA = %d\n", (a * b - c * d));
+	return 0;
+}
