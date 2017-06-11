f61afe91c99ffed68b9615b7cba8b50d387733b4
Rafael Giordanno <rafael_giordanno@hotmail.com>
2017-06-11 17:48:23 -0300

    Problem 1004 solved.

---
+++ c/01-beginner/1004.c
@@ -0,0 +1,13 @@
+/** 
+ * link: https://www.urionlinejudge.com.br/judge/en/problems/view/1004
+ * Author: Rafael Giordanno
+ **/
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("PROD = %d\n", (a * b));
+	return 0;
+}
