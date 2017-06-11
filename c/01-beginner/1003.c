97b8612463082ad688beef51a3a9eb17002bddd3
Rafael Giordanno <rafael_giordanno@hotmail.com>
2017-06-11 13:43:21 -0300

    Problem 1003 solved.

---
+++ c/01-beginner/1003.c
@@ -0,0 +1,13 @@
+/** 
+ * link: https://www.urionlinejudge.com.br/judge/en/problems/view/1003
+ * Author: Rafael Giordanno
+ **/
+#include <stdio.h>
+
+int main()
+{
+	int A, B;
+	scanf("%d %d", &A, &B);
+	printf("SOMA = %d\n", (A + B));
+	return 0;
+}
