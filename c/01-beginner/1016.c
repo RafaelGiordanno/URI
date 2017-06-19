4e3ffebb717d0f222ee93a39458ba9f2515512bb
Rafael Giordanno <rafael_giordanno@hotmail.com>
2017-06-19 17:57:54 -0300

    Problem 1016 solved in C.

---
+++ c/01-beginner/1016.c
@@ -0,0 +1,13 @@
+/**
+ * link: https://www.urionlinejudge.com.br/judge/en/problems/view/1016
+ * Author: Rafael Giordanno
+ **/
+#include <stdio.h>
+
+int main()
+{
+	int d;
+	scanf("%d", &d);
+	printf("%d minutos\n", (d * 2));
+	return 0;
+}
